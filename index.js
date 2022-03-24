c809ed6092db8f96ddc141c4e430499dee618b91
Shr3yashhh <joshi.shreyash1@gmail.com>
2022-03-24 20:00:12 +0545

    node parc

---
+++ index.js
@@ -0,0 +1,3 @@
+const conn_pg = require('./loder')
+console.log(conn_pg);
+// console.log(arr);
---
+++ loder.js
@@ -0,0 +1,11 @@
+const arr = ['shreyash', 'shrestha', 'developer', 'cricket'];
+const ages = [23,22,21,24];
+console.log(arr);
+// export (arr)
+module.exports = 
+{
+    arr : arr,
+    ages : ages
+}
+
+// {arr, ages}
