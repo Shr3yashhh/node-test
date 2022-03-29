4aec5fc1f95635f5b9c14025f89da5d9425c508e
Shr3yashhh <joshi.shreyash1@gmail.com>
2022-03-29 12:28:11 +0545

    git ignore file added

--- stream.js
+++ stream.js
@@ -1,5 +1,12 @@
 const fs = require('fs')
 
-const readStream = fs.createReadStream('./docs/blog3.txt', { encoding : 'utf8'});
+const readStream = fs.createReadStream('./docs/blog3.txt');
 const writeStream = fs.createWriteStream('./docs/blog3.txt');
 
+readStream.on('data', (chunk) => {
+    console.log('----- NEW CHUNK -----');
+    console.log(chunk);
+})
+
+
+// console.log(readStream);
